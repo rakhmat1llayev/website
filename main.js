b0cc0c7bfb9b12de5184361f79309f91d9d22f3b
rakhmat1llayev <raxmatullayevulugbek2797@gmail.com>
2023-01-12 17:41:05 -0700

    header web site

---
+++ main.js
@@ -0,0 +1,9 @@
+const navbarList = document.querySelector('.lists')
+
+const hamburger = document.getElementById('hamburger')
+
+hamburger.addEventListener('click', () => {
+  navbarList.classList.toggle('active-navbar')
+
+})
+const elem = document.getElementById("animation");
